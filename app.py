facdcbc240948d2cde3309fc6cee36c9beb59a63
Iyus  Rustandi <rustandi@Meyneth.local>
2021-10-12 22:22:08 +0700

    get client ip and hit counts

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+# compose_flask/app.py
 import flask
+from redis import Redis
 
 app = flask.Flask(__name__)
+redis = Redis(host='redis', port=6379)
 
-@app.route("/")
-def index():
+@app.route('/')
+def hello():
+    # redis.incr('hits')
+    # return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
     ip_address = flask.request.remote_addr
-    return "User address: " + ip_address
+    visits = redis.incr('counter')
+    html = "<h3>User address: {ip_address}</h3>" \
+           "<b>Hits:</b> {visits}" \
+           "<br/>"
+    return html.format(visits=visits, ip_address=ip_address)
 
-app.run(host="0.0.0.0", port=80)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
